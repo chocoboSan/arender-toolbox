509537237472a904655cd9100fcb3e6e1484f8d8
Maxime Veron <maximveron@gmail.com>
2020-03-17 15:07:20 +0100

    Version 1.0.5, attempt using env property named sun.net.inetaddr.ttl

--- lb-spring-boot-tester/lb-spring-boot-tester-client/src/main/java/net/unicornbox/lbspringboottesterclient/LbSpringBootTesterClientApplication.java
+++ lb-spring-boot-tester/lb-spring-boot-tester-client/src/main/java/net/unicornbox/lbspringboottesterclient/LbSpringBootTesterClientApplication.java
@@ -6,10 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LbSpringBootTesterClientApplication {
 
-    static {
-        java.security.Security.setProperty("networkaddress.cache.ttl", "0");
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(LbSpringBootTesterClientApplication.class, args);
     }
