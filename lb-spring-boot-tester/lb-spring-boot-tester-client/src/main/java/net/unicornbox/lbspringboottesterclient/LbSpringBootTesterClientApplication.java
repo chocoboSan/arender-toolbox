20409d954aa14d438972ba7ed5fd8d0b98aa8010
Maxime Veron <maximveron@gmail.com>
2020-03-17 14:57:17 +0100

    Version 1.0.4, forcing again to have static block de-activating cache.

--- lb-spring-boot-tester/lb-spring-boot-tester-client/src/main/java/net/unicornbox/lbspringboottesterclient/LbSpringBootTesterClientApplication.java
+++ lb-spring-boot-tester/lb-spring-boot-tester-client/src/main/java/net/unicornbox/lbspringboottesterclient/LbSpringBootTesterClientApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LbSpringBootTesterClientApplication {
 
     static {
+        java.security.Security.setProperty("networkaddress.cache.ttl", "0");
     }
 
     public static void main(String[] args) {
